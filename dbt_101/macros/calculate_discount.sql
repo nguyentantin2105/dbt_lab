9ea08c5e1cb3aa612cfd42e694e61363d99d27d1
dp-airflow-integration <ducltm@technify.vn>
2025-05-24 10:32:44 +0700

    update remove whitespace

--- dbt_101/macros/calculate_discount.sql
+++ dbt_101/macros/calculate_discount.sql
@@ -1,6 +1,6 @@
 {% macro calculate_discount(order_count, total_amount_spent, scale=2) %}
     case
-        when {{order_count}} <= 3 then (0.03 * {{total_amount_spent}})::numeric(16, {{ scale }})
-        else (0.05 * {{total_amount_spent}})::numeric(16, {{ scale }})
+        when {{ order_count }} <= 3 then (0.03 * {{ total_amount_spent }})::numeric(16, {{ scale }})
+        else (0.05 * {{ total_amount_spent }})::numeric(16, {{ scale }})
     end
 {% endmacro %}
--- dbt_101/macros/get_discount_eligibility.sql
+++ dbt_101/macros/get_discount_eligibility.sql
@@ -1,6 +1,6 @@
 {% macro get_discount_eligibility(order_count, total_amount_spent) %}
     case
-        when {{order_count}} >= 3 OR {{total_amount_spent}} >= 300 then TRUE
+        when {{ order_count }} >= 3 OR {{ total_amount_spent }} >= 300 then TRUE
         else FALSE
     end
 {% endmacro %}
